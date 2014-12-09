0780eba1017e11db1295dd81a792cb750b49e711
devnoname120 <devnoname120+gh@gmail.com>
2014-12-09 17:49:01 +0100

    Fix Hugefiles (now works with www subdomain)

--- AntiAdware.user.js
+++ AntiAdware.user.js
@@ -12,7 +12,7 @@
 // @description:de Mit AntiAdware vermeidest du auf zahlreichen Webseiten den versehentlichen Download von unerwünschten Programmen
 // @description:zh-TW AntiAdware, 讓你避免在許多網站上意外下載到廣告軟體.
 // @description:zh-CN AntiAdware, 让你避免在许多网站上意外下载到广告软体.
-// @version 1.26.2
+// @version 1.26.3
 // @license Creative Commons BY-NC-SA
 
 // jQuery dependency; an offline version of this is included in the script in case it goes down
@@ -41,7 +41,7 @@
 // @include http://*freewarefiles.com/downloads_counter.php*
 // @include http://*google.*/intl/*/earth/download/ge/agree.html
 // @include http://hipfile.com/*
-// @include http://hugefiles.net/*
+// @include http://*hugefiles.net/*
 // @include http://hulkload.com/*
 // @include http://*kingfiles.net/*
 // @include http*://*letitbit.net/download/*
