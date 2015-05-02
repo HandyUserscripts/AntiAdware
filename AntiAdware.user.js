f2f2e26c7f57e138781dd401a9682bba23d5082e
devnoname120 <devnoname120+gh@gmail.com>
2015-05-02 17:01:11 +0200

    Bump version to 1.33.0

--- AntiAdware.user.js
+++ AntiAdware.user.js
@@ -12,7 +12,7 @@
 // @description:de Mit AntiAdware vermeidest du auf zahlreichen Webseiten den versehentlichen Download von unerwünschten Programmen
 // @description:zh-TW AntiAdware, 讓你避免在許多網站上意外下載到廣告軟體.
 // @description:zh-CN AntiAdware, 让你避免在许多网站上意外下载到广告软体.
-// @version 1.32.0
+// @version 1.33.0
 // @license Creative Commons BY-NC-SA
 
 // jQuery dependency; an offline version of this is included in the script in case it goes down
